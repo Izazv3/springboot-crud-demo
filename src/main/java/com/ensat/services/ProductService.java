3374e92b0303f6d30b9f09e4be787ced479f0fb6
Izaz Ahamed <92938708+Izazv3@users.noreply.github.com>
2026-09-20 21:54:39 +0530

    Implement PostgreSQL CRUD: ProductService.java

--- src/main/java/com/ensat/services/ProductService.java
+++ src/main/java/com/ensat/services/ProductService.java
@@ -1,15 +1,10 @@
 package com.ensat.services;
-
 import com.ensat.entities.Product;
-
+import java.util.List;
 public interface ProductService {
-
-    Iterable<Product> listAllProducts();
-
-    Product getProductById(Integer id);
-
-    Product saveProduct(Product product);
-
-    void deleteProduct(Integer id);
-
+ List<Product> listAllProducts();
+ Product getProductById(Integer id);
+ Product saveProduct(Product product);
+ Product updateProduct(Integer id,Product product);
+ void deleteProduct(Integer id);
 }
